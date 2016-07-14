1360b0567467355801af70a788b44d38ade18a48
Mark P <priceoner+marcfoxx@gmail.com>
2016-07-14 03:01:08 -0700

    commit.

--- resources/views/leadInput.blade.php
+++ resources/views/leadInput.blade.php
@@ -12,22 +12,28 @@
 <input name="infusionsoft_version" type="hidden" value="1.45.0.48" />
 
 
-                        <div class="input-group">
+<div class="form-group input-group">
+                <span class="input-group-addon">@</span>
+                <input class="form-control" placeholder="Username" type="text">
+            </div>
+
+
+                        <div class="form-group input-group">
                           <span class="input-group-addon" id="basic-addon1"><i class="fa fa-user fa-2"></i></span>
                           <input type="text" class="form-control" placeholder="Name" aria-describedby="basic-addon1" id="inf_field_FirstName" name="inf_field_FirstName" required="required">
                         </div>
 
-                          <div class="input-group">
+                          <div class="form-group input-group">
                           <span class="input-group-addon" id="basic-addon1"><i class="fa fa-envelope fa-2"></i></span>
                           <input type="text" class="form-control" placeholder="Email Address" aria-describedby="basic-addon1" id="inf_field_Email" name="inf_field_Email" required="required" validate="">
                         </div>
 
-                        <div class="input-group">
+                        <div class="form-group input-group">
                           <span class="input-group-addon" id="basic-addon1"><i class="fa fa-phone fa-3"></i></span>
                           <input type="text" class="form-control" placeholder="Phone (Optional)" aria-describedby="basic-addon1" id="inf_field_Phone1" name="inf_field_Phone1">
                         </div>
 
-                        <div class="input-group">
+                        <div class="form-group input-group">
                           <span class="input-group-addon" id="basic-addon1"><i class="fa fa-comment-o fa-3"></i></span>
                           <textarea class="form-control" placeholder="Questions or Comments" aria-describedby="basic-addon1"  id="inf_custom_QuestionsComments" name="inf_custom_QuestionsComments" rows="3"></textarea>
 
@@ -43,12 +49,12 @@
   <div class="col-lg-6">
                     <h2>Scan Company by Website</h2>
               
-                          <div class="input-group">
+                          <div class="form-group input-group">
                           <span class="input-group-addon" id="basic-addon1"><i class="fa fa-envelope fa-2"></i></span>
                           <input type="text" class="form-control" placeholder="Email Address" aria-describedby="basic-addon1" id="inf_field_Email" name="inf_field_Email" required="required" validate="">
                           </div>
 
-                          <div class="input-group">
+                          <div class="form-group input-group">
                           <span class="input-group-addon" id="basic-addon1"><i class="fa fa-globe fa-2"></i></span>
                           <input type="text" class="form-control" placeholder="Website" aria-describedby="basic-addon1" id="inf_field_Website" name="inf_field_Website" required="required" validate="">
                           </div>
